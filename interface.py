eca5a9ba5f90e59e44444b8d81e2a142f334c780
Tanger <tangerluo@qq.com>
2024-03-12 22:27:51 +0800

    the first commit

---
+++ interface.py
@@ -0,0 +1,33 @@
+import gradio as gr
+import cv2
+from paddleocr import PaddleOCR, draw_ocr
+
+
+'''input_list = {
+    gr.File(type="binary", label="File"),
+    gr.Dataframe(label="Dataframe"),
+}'''
+
+
+def file_convert(file):
+    # 处理逻辑
+    # 读取图像
+    # 使用默认模型路径
+    paddleocr = PaddleOCR(lang='ch', show_log=False)
+    img = cv2.imread(file)  # 打开需要识别的图片
+    result = paddleocr.ocr(img)
+    alist = [None] * len(result[0])  # 创建一个与result[0]相同长度的空列表
+    for i in range(len(result[0])):
+        print(result[0][i][1][0])  # 输出识别结果
+        alist[i] = tuple(result[0][i][1][0])  # 将识别结果存储到alist中
+
+    '''
+    # 打印检测结果
+    for line in result:
+        print(line)'''
+
+    return alist
+
+
+iface = gr.Interface(file_convert, gr.File(), gr.Dataframe(), title="表格转换器", live=True,)
+iface.launch()
